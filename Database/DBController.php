6f02760043b9cda7243a04139a66ce06d730ab9c
amine-khemira <66252468+amine-khemira@users.noreply.github.com>
2022-10-10 23:02:16 +0200

    Add files via upload

--- Database/DBController.php
+++ Database/DBController.php
@@ -12,10 +12,11 @@ class DBController
     public function __construct()
     {
         $this->con=mysqli_connect($this->host,$this->user,$this->password,$this->database);
+
         if ($this->con->connect_error){
             echo"fail".$this->con->connect_error;
         }
-        echo 'connection successful';
+
     }
 
 
--- Database/Product.php
+++ Database/Product.php
@@ -25,5 +25,70 @@ class Product
         }
         return $resultArray;
     }
+    public function getProduct($item_id=null,$table='produit'){
+        if(isset($item_id)){
+            $result=$this->db->con->query("SELECT * FROM produit INNER JOIN fabriquant ON produit.id_fab = fabriquant.id_fab WHERE id_produit={$item_id}");
+        }
+        $resultArray =array();
+        while($item=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $resultArray[]=$item;
+        }
+        return $resultArray;
+    }
+    public function getProduct2($item_id=null,$table='produit'){
+        if(isset($item_id)){
+            $result=$this->db->con->query("SELECT * FROM {$table} WHERE id_produit={$item_id}; ");
+        }
+        $resultArray =array();
+        while($item=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $resultArray[]=$item;
+        }
+        return $resultArray;
+    }
+    public function getDatapan($table='commande'){
+        $result=$this->db->con->query("SELECT id_produit,id_fab,id_user FROM {$table} WHERE id_user={$_SESSION['userID']} ");
+        $resultArray =array();
+        while($item=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $resultArray[]=$item;
+        }
+        return $resultArray;
+    }
+    public function getData2($table='produit'){
+        $result=$this->db->con->query("SELECT * FROM {$table} WHERE id_user={$_SESSION['userID']}");
+        $resultArray =array();
+        while($item=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $resultArray[]=$item;
+        }
+        return $resultArray;
+    }
+    public function getProduct3($item_id=null,$table='produit'){
+        if(isset($item_id)){
+            $result=$this->db->con->query("SELECT * FROM {$table} WHERE id_fab={$item_id}; ");
+        }
+        $resultArray =array();
+        while($item=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $resultArray[]=$item;
+        }
+        return $resultArray;
+    }
+    public function getData3($table='produit',$id_f=null){
+        $result=$this->db->con->query("SELECT * FROM {$table} WHERE id_fab={$id_f}");
+        $resultArray =array();
+        while($item=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $resultArray[]=$item;
+        }
+        return $resultArray;
+    }
+    public function getData4($id=null){
+        $result=$this->db->con->query("SELECT u.adresse_user,c.id_commande,c.date,p.nom_produit,p.Prix_produit,c.status FROM produit p,commande c,user u WHERE p.id_produit=c.id_produit AND c.id_user=u.id_user AND c.id_fab={$id};");
+        $resultArray =array();
+        while($item=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $resultArray[]=$item;
+        }
+        return $resultArray;
+    }
+
+
+
 
 }
